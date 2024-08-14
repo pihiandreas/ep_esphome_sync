84b0e1f38440842cec4443c4ba24249d9a3a33be
Andreas Berg <andreas@edupihi.eu>
2024-08-14 14:08:26 +0300

    ade79553 dev

--- esphome/components/ade7953_base/ade7953_base.cpp
+++ esphome/components/ade7953_base/ade7953_base.cpp
@@ -116,6 +116,7 @@ void ADE7953::dump_config() {
   ESP_LOGCONFIG(TAG, "  BIGAIN_32: 0x%08jX", (uintmax_t) bigain_);
   ESP_LOGCONFIG(TAG, "  AWGAIN_32: 0x%08jX", (uintmax_t) awgain_);
   ESP_LOGCONFIG(TAG, "  BWGAIN_32: 0x%08jX", (uintmax_t) bwgain_);
+  ESP_LOGCONFIG(TAG, "  ACCMODE_32: 0x%08jX", (uintmax_t) accmode_);
 }
 
 template<typename F>
