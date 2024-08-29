742e600bc4e19014ae562651c81cabd0ea3d8ba7
Andreas Berg <andreas@edupihi.eu>
2024-08-29 20:47:23 +0300

    mcp23s17: support esp-idf spi

--- esphome/components/mcp23s17/mcp23s17.h
+++ esphome/components/mcp23s17/mcp23s17.h
@@ -9,7 +9,7 @@ namespace esphome {
 namespace mcp23s17 {
 
 class MCP23S17 : public mcp23x17_base::MCP23X17Base,
-                 public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_HIGH, spi::CLOCK_PHASE_TRAILING,
+                 public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING,
                                        spi::DATA_RATE_8MHZ> {
  public:
   MCP23S17() = default;
